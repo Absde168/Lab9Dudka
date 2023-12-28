de5ce3d5a181b970f9bd7ef446d247c75e01dd86
B3dRandoms <bdrandoms0@gmail.com>
2023-12-28 02:19:47 +0200

    Initial commit

---
+++ Lab9Dudka/Program.cs
@@ -0,0 +1,45 @@
+﻿class Parallelepiped
+{
+    private double length;
+    private double width;
+    private double height;
+    public Parallelepiped()
+    {
+        length = 0;
+        width = 0;
+        height = 0;
+    }
+    public Parallelepiped(double l, double w, double h)
+    {
+        length = l;
+        width = w;
+        height = h;
+    }
+    ~Parallelepiped()
+    {
+        Console.WriteLine("Объект параллелепипеда уничтожен");
+    }
+    public double CalculateVolume()
+    {
+        return length * width * height;
+    }
+    public double CalculateDiagonal()
+    {
+        return Math.Sqrt(length * length + width * width + height * height);
+    }
+    public string GetInfo()
+    {
+        return $"Параллелепипед с длинами сторон: {length}, {width}, {height}";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Parallelepiped p1 = new Parallelepiped(); 
+        Parallelepiped p2 = new Parallelepiped(3, 4, 5);
+        Console.WriteLine(p2.GetInfo());
+        Console.WriteLine($"Объем параллелепипеда: {p2.CalculateVolume()}");
+        Console.WriteLine($"Длина наибольшей диагонали параллелепипеда: {p2.CalculateDiagonal()}");
+    }
+}
